4497842566b1b7f695bd8fed5dba5805632a94ff
HlebNoGleb <gynyax5@gmail.com>
2018-11-01 18:47:02 +0300

    preloader

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+document.body.onload = function(){
+	setTimeout(function(){
+		var preloader = document.getElementById('page-preloader');
+		if(!preloader.classList.contains('done')){
+			preloader.classList.add('done');
+		}
+	}, 1000);
+}
 $('.carusel').slick({
 	arrows: true,
 	dots: false,
